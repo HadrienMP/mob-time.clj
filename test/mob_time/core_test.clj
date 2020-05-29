c916bfdb1df8b84cba25b51eac82d6fa1b36c874
tployon <tployon@financeactive.com>
2020-05-29 15:57:42 +0200

    fix test

--- test/mob_time/core_test.clj
+++ test/mob_time/core_test.clj
@@ -1,3 +1,15 @@
-Error detected while processing function cljfmt#AutoFormat[3]..cljfmt#Format[10]..<SNR>70_GetFormattedFile:
-line    4:
-E117: Unknown function: fireplace#session_eval
+(ns mob-time.core-test
+  (:require [clojure.test :refer :all]
+            [mob-time.core :refer :all]
+            [cheshire.core :as json :refer :all]
+            [clj-http.client :refer :all]))
+
+(def timeLeftInMillis [url]
+  (json/parse-string
+    (:body (clj-http.client/get (str mob-url "/fwg/status"))))
+  )
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 (timeLeftInMillis "https://mob-time-server.herokuapp.com")))))
+; https://mob-time-server.herokuapp.com/fwg/status
+; {"lengthInMinutes":4,"timeLeftInMillis":90200,"pomodoro":{"ratio":0.6497}
