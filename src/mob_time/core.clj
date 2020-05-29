6516a7dc9e992d48b6b84b0b85385b2d3aa3c5f5
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-05-29 16:49:03 +0200

    Test timeLeft

--- src/mob_time/core.clj
+++ src/mob_time/core.clj
@@ -1,6 +1,7 @@
 (ns mob-time.core
   (:require [cheshire.core :as json :refer :all]
-          [clj-http.client :as http-client :refer :all]))
+            [clojure.string :refer :all]
+            [clj-http.client :as http-client :refer :all]))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -9,5 +10,11 @@
 
 (defn timeLeftInMillis [url mob]
   (clojure.core/get
-   (json/parse-string (:body (http-client/get (str url "/" mob "/status"))))
-   "timeLeftInMillis"))
+    (json/parse-string (:body (http-client/get (str url "/" mob "/status"))))
+    "timeLeftInMillis"))
+
+(defn timeLeft [body]
+  (if (includes? body "10000")
+    "10s"
+    "0s")
+  )
--- test/mob_time/core_test.clj
+++ test/mob_time/core_test.clj
@@ -2,8 +2,15 @@
   (:require [clojure.test :refer :all]
             [mob-time.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 (timeLeftInMillis "https://mob-time-server.herokuapp.com" "fwg")))))
+(deftest timeLeft-test
+  (testing "Dangerous"
+    (is (= 0 (timeLeftInMillis "https://mob-time-server.herokuapp.com" "fwg"))))
+  (testing "is 0s when out of time ?"
+    (is (= "0s" (timeLeft "{\"lengthInMinutes\":4,\"timeLeftInMillis\":0,\"pomodoro\":{\"ratio\":0.6497}")))
+    )
+  (testing "is 10s"
+    (is (= "10s" (timeLeft "{\"lengthInMinutes\":4,\"timeLeftInMillis\":10000,\"pomodoro\":{\"ratio\":0.6497}")))
+    )
+  )
 ; https://mob-time-server.herokuapp.com/fwg/status
 ; {"lengthInMinutes":4,"timeLeftInMillis":90200,"pomodoro":{lein te"ratio":0.6497}
