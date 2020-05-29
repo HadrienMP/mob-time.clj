eef91cc97bafb175a6a9dc49e5cdcd9e138fad66
Thomas Carpaye <tcarpaye@financeactive.com>
2020-05-29 16:31:54 +0200

    Move timeLeftInMillis to production code

--- src/mob_time/core.clj
+++ src/mob_time/core.clj
@@ -1,7 +1,13 @@
 (ns mob-time.core
-  (:gen-class))
+  (:require [cheshire.core :as json :refer :all]
+          [clj-http.client :as http-client :refer :all]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn timeLeftInMillis [url]
+  (clojure.core/get
+   (json/parse-string (:body (http-client/get (str url "/fwg/status"))))
+   "timeLeftInMillis"))
--- test/mob_time/core_test.clj
+++ test/mob_time/core_test.clj
@@ -1,13 +1,6 @@
 (ns mob-time.core-test
   (:require [clojure.test :refer :all]
-            [mob-time.core :refer :all]
-            [cheshire.core :as json :refer :all]
-            [clj-http.client :as http-client :refer :all]))
-
-(defn timeLeftInMillis [url]
-  (clojure.core/get
-    (json/parse-string (:body (http-client/get (str url "/fwg/status"))))
-    "timeLeftInMillis"))
+            [mob-time.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
